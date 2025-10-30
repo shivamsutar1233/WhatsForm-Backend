889e3dea9f95180dd933e3efe394f3b4021c9e33
Shivam Sutar <shivamsutar33@hotmail.com>
2025-10-30 12:03:04 +0530

    COARS fix

--- api/index.js
+++ api/index.js
@@ -16,7 +16,13 @@ config({ path: "./.env" });
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Serve static files in production
